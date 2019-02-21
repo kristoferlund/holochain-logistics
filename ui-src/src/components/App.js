5de316e8595ce63652ac785189ed1b233c10b942
Kristofer Lund <kristofer@fmckl.se>
2019-02-21 22:57:30 +0100

    Updated texts

--- ui-src/src/components/App.js
+++ ui-src/src/components/App.js
@@ -4,13 +4,13 @@ import React from 'react'
 export const Title = () => {
   return (
     <div className='title'>
-      <h1 className='f1 lh-copy'>Holochain Logistics (4000?)</h1>
+      <h1 className='f1 lh-copy'>Hologistics 4001.</h1>
 
-      <p className='f5 f4-ns measure lh-copy'><Link to='/products'>Products</Link> – Manage the global product list, common for all profiles </p>
+      <p className='f5 f4-ns measure lh-copy'><Link to='/products'>Products</Link> – Manage the global product list, common for all user profiles </p>
 
-      <p className='f5 f4-ns measure lh-copy'><Link to='/inventory'>Inventory</Link> – Manage the inventory linked to your profile [not done]</p>
+      <p className='f5 f4-ns measure lh-copy'><Link to='/inventory'>Inventory</Link> – Manage the inventory linked to your profile <b>[not ready]</b></p>
 
-      <p className='f5 f4-ns measure lh-copy'><Link to='/orders'>Orders</Link> – Browse other users inventory, place orders, sign to send and receive  [not done]</p>
+      <p className='f5 f4-ns measure lh-copy'><Link to='/orders'>Orders</Link> – Browse other users inventory, place orders, sign to send and receive  <b>[not ready]</b></p>
 
     </div>
   )
